42681f0bde5e261849801cf79ce42e7225d0b69c
R-hosseini <rayehe_hosseyni@yahoo.com>
2017-01-02 01:23:50 +0330

    farsnet init

---
+++ SemEval/src/semeval/FnServices.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semeval;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+import edu.mit.jwi.item.ISynset;
+import ir.sbu.nlp.wordnet.data.model.FNSense;
+import ir.sbu.nlp.wordnet.data.model.FNSynset;
+import ir.sbu.nlp.wordnet.service.FNSynsetService; 
+/**
+ *
+ * @author ArOmA
+ */
+public class FnServices {
+    
+    public static void relations()
+    {
+        System.out.println("hiii");
+    }
+    
+}
